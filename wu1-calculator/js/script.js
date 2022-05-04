db59d372ff7e8b669e385df93873345b8be81b5a
MelwinHeimby <104508905+MelwinHeimby@users.noreply.github.com>
2022-05-04 15:59:29 +0200

    Update script.js

--- wu1-calculator/js/script.js
+++ wu1-calculator/js/script.js
@@ -1,10 +1,69 @@
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-      
+let lcd = null; // displayen
+
+let memory = 0; // Lagrat/gamlat värdet från display
+let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
+
+function init() {
+    lcd = document.getElementById('lcd');
+    let keyBoard = document.getElementById('keyBoard')
+    keyBoard.onclick = buttonClick;
+}
+
+/**
+ * Händelsehanterare för kalkylatorns tangentbord
+ */
+function buttonClick(e) {
+    let btn = e.target.id; //id för den tangent som tryckte ner
+
+
+    // kollar om siffertangent är nedtryckt
+    if (btn.substring(0, 1) === 'b') {
+        let digit = btn.substring(1, 2); // plockar ut siffran från id:et
+
+    } else { // Inte en siffertangent, övriga tangenter.
+
     }
-   })
+}
 
+/**
+ *  Lägger till siffra på display.
+ */
+function addDigit(digit) {
+}
 
-const keys = (test) =>  {
-    console.log(test);
-       }
+/**
+ * Lägger till decimaltecken
+ */
+function addComma() {
+
+}
+
+/**
+ * Sparar operator.
+ * +, -, *, /
+ */
+function setOperator(operator){
+
+}
+
+/**
+ * Beräknar ovh visar resultatet på displayen.
+ */
+function calculate() {
+
+}
+
+/** Rensar display */
+function clearLCD() {
+    lcd.value = '';
+    isComma = false;
+}
+
+/** Rensar allt, reset */
+function memClear(){
+    memory = 0;
+    arithmetic = null;
+    clearLCD();
+}
+
+window.onload = init;
