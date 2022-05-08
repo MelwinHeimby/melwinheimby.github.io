060c2ee2a904e4ae0ded50058fddb85d5e86f4ad
MelwinHeimby <04heme@skola.engelholm.se>
2022-05-08 13:59:18 +0200

    Operator

--- wu1-calculator/js/script.js
+++ wu1-calculator/js/script.js
@@ -4,66 +4,54 @@ let memory = 0; // Lagrat/gamlat värdet från display
 let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
 
 function init() {
-    lcd = document.getElementById('lcd');
-    let keyBoard = document.getElementById('keyBoard')
-    keyBoard.onclick = buttonClick;
+  lcd = document.getElementById("lcd");
+  let keyBoard = document.getElementById("keyBoard");
+  keyBoard.onclick = buttonClick;
 }
 
 /**
  * Händelsehanterare för kalkylatorns tangentbord
  */
 function buttonClick(e) {
-    let btn = e.target.id; //id för den tangent som tryckte ner
+  let btn = e.target.id; //id för den tangent som tryckte ner
 
-
-    // kollar om siffertangent är nedtryckt
-    if (btn.substring(0, 1) === 'b') {
-        let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-
-    } else { // Inte en siffertangent, övriga tangenter.
-
-    }
+  // kollar om siffertangent är nedtryckt
+  if (btn.substring(0, 1) === "b") {
+    let digit = btn.substring(1, 2); // plockar ut siffran från id:et
+  } else {
+    // Inte en siffertangent, övriga tangenter.
+  }
 }
 
-/**
- *  Lägger till siffra på display.
- */
-function addDigit(digit) {
-}
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): '); 
 
 /**
  * Lägger till decimaltecken
  */
-function addComma() {
-
-}
+function addComma() {}
 
 /**
  * Sparar operator.
  * +, -, *, /
  */
-function setOperator(operator){
-
-}
+function setOperator(operator) {}
 
 /**
  * Beräknar ovh visar resultatet på displayen.
  */
-function calculate() {
-
-}
+function calculate() {}
 
 /** Rensar display */
 function clearLCD() {
-    lcd.value = '';
-    isComma = false;
+  lcd.value = "";
+  isComma = false;
 }
 
 /** Rensar allt, reset */
-function memClear(){
-    memory = 0;
-    arithmetic = null;
-    clearLCD();
+function memClear() {
+  memory = 0;
+  arithmetic = null;
+  clearLCD();
 }
 
 window.onload = init;
