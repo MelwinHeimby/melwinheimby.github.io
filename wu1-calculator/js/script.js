652000fe0ec7745154a421859f275924b98b8c06
MelwinHeimby <04heme@skola.engelholm.se>
2022-05-15 14:47:13 +0200

    Fix Every

--- wu1-calculator/js/script.js
+++ wu1-calculator/js/script.js
@@ -1,3 +1,18 @@
+function display(val) {
+  document.getElementById("result").value += val;
+  console.log
+  return val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+
+  let y = eval(x);
+
+  document.getElementById("result").value = y;
+
+  return y;
+}
 
 function clearScreen(){
 
